3ecd4e79a00c4454ff9fcdfbc25cadc24ab0d47a
Ben Hosmer <ben@radarearth.com>
2011-09-02 14:15:45 -0400

    Added PHP version

---
+++ 99bottles.php
@@ -0,0 +1,12 @@
+<?php
+$bottles = 99;
+  while ($bottles > 0) {
+    echo "$bottles bottles of beer on the wall, ";
+    echo "$bottles bottles of beer,";
+    echo "take one down and pass it around...</br>";
+    $bottles--;
+} 
+if($bottles > 1) {
+echo "</p>Time to buy more beer!";
+}
+?>
