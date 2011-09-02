298794ef8721b0be2839d9f04694cfac8ea813fc
Ben Hosmer <ben@radarearth.com>
2011-09-02 14:14:20 -0400

    Initial commit of python version

---
+++ 99bottles.py
@@ -0,0 +1,7 @@
+numofbottles = 99
+while numofbottles >= 1:
+  print str(numofbottles) + " bottles of beer on the wall, " 
+  print str(numofbottles) + " bottles of beer."
+  print "Take one down and pass it around..."
+  numofbottles = numofbottles - 1
+print "Time to buy more beer!"
