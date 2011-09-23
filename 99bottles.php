7c2de0d37af925e1150a867505c389afc5ee9b8f
Ben Hosmer <ben.hosmer@gmail.com>
2011-09-23 08:23:37 -0300

    Added a space after output on line 4

--- 99bottles.php
+++ 99bottles.php
@@ -2,7 +2,7 @@
 $bottles = 99;
   while ($bottles > 0) {
     echo "$bottles bottles of beer on the wall, ";
-    echo "$bottles bottles of beer,";
+    echo "$bottles bottles of beer, ";
     echo "take one down and pass it around...</br>";
     $bottles--;
 } 
