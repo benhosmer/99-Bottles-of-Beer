bf0949468f47c2a3885cd7b9ca8c36d519d11cf9
Ben Hosmer <ben@radarearth.com>
2011-10-18 18:16:27 -0400

    Adding dart language file - not quite working yet.

---
+++ 99bottles.dart
@@ -0,0 +1,11 @@
+main() {
+  int number1 = 99;
+  print('${number1}' + ' bottles of beer on the wall,');
+  print('${number1}' + ' bottles of beer.');
+  print('Take one down and pass it around,');
+  int number2 = number1 - 1;
+  print('${number2}' + ' bottles of beer on the wall');
+
+  int othernum = 2;
+  while (othernum > 0)
+}
